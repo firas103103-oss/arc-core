997c60feefe2aa8d439580dfb35f86a8fbaa1f5d
firas103103-oss <firas103103@gmail.com>
2025-12-31 01:57:35 +0000

    add protocols endpoint

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
+const PROTOCOLS_DIR = path.join(__dirname, "protocols");
 
 app.get("/", (_, res) => res.send("ARC OK"));
 app.get("/health", (_, res) => res.send("ARC OK"));
+
+app.get("/protocols", (_, res) => {
+	if (!fs.existsSync(PROTOCOLS_DIR)) return res.json([]);
+	const files = fs.readdirSync(PROTOCOLS_DIR);
+	res.json(files);
+});
 
 app.listen(8080, () => console.log("RUNNING 8080"));
