195182187d89d10294720abe1b476144ed8e20bb
firas103103-oss <firas103103@gmail.com>
2025-12-31 01:55:42 +0000

    arc brain health confirmed

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (_, res) => res.send("ARC OK"));
 app.get("/health", (_, res) => res.send("ARC OK"));
+
 app.listen(8080, () => console.log("RUNNING 8080"));
